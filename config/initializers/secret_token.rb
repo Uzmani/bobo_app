bb409c5d3a833deb1b13ffeeafc57a40ca20e8ac
uzmani <uzmani@yahoo.com>
2013-11-22 11:58:07 -0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bobo::Application.config.secret_key_base = 'f27e1856dbda95bcbd9e06fdddfe9f06151643625a7c1a0cb155077520e4d7f86c6ad16e751c6c3a4655ea428f60a1a33780bf9e3225fe8782161404ceed8026'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bobo::Application.config.session_store :cookie_store, key: '_bobo_session'
